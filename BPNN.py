6db320c26144496e75f64012d42a0dde8fe71b8a
jprili <rilijames@gmail.com>
2024-04-10 12:53:07 -0700

    modified call() to use tensors

--- BPNN.py
+++ BPNN.py
@@ -23,70 +23,32 @@ class BPNN(keras.Model):
     The subnet takes all the atom coordinates,
     and outputs the atomic contribution of energy
     """
-    def __init__(self, layers: list, r_c, r_s, eta1, eta2, lbd, zeta):
+    def __init__(self, layers: list, num_syms = 14):
         super().__init__()
 
         # properties of the subnet
-        self._input  = Input(shape = (2, ))
-        self.layers  = layers
-        self._output = Dense(1, activation = "softmax")
-        
-        self.r_c  = r_c   # angstrom
-        self.r_s  = r_s 
-        self.eta1 = eta1
-        self.eta2 = eta2
-        self.lbd  = 1 if lbd == 1 else -1
-        self.zeta = zeta
-
-    def _norm(self, r):
-        """
-        macro for 2-d norm
-        """
-        return np.sqrt(np.dot(r, r))
-
-    def _cut_off(self, r: float) -> float:
-        """
-        radial cutoff function
-        """
-        out = 0
-        if r <= self.r_c:
-            out = 0.5 * np.cos(np.pi * (r/self.r_c) + 1)
-    
-        return out
-
-    def _trim_padding(self, inputs):
-        mask = tf.reduce_any(inputs != -99, axis=-1)
-        mask = tf.cast(mask, dtype=inputs.dtype)
-               
-        return inputs * tf.expand_dims(mask, axis=-1)
+        self.subnet   = Sequential([
+            Input(shape = (num_syms,)),
+            *layers
+        ])
+        self.num_syms = num_syms
 
     def call(self, inputs, training = False):
         """
         Feed-forward algorithm for the model.
-        calculate the values of the symmetry functions
+        split each row and feed to the subnet
         for each atom and applies to the same layers.
         """
-        
-        inputs = self._trim_padding(inputs)
-        e_curr = 0
-        g1s = self.get_g1is(inputs)
-        g2s = self.get_g2is(inputs)
+        # splits the rows
+        syms = tf.unstack(inputs, axis = 1)
 
-        for idx, in_vec in enumerate(zip(g1s, g2s)):
-            in_vec = np.array(in_vec)
-            x = self._input(in_vec)
-            
-            for layer in self.layers:
-                # dropout only applies on training
-                # for reqularisation
-                if isinstance(layer, Dropout):
-                    x = layer(x, training = training)
-                    
-                x = layer(x)
+        sym_e_contribution = []
+        for sym in syms:
+            subnet_out = self.subnet(sym, training = training)
+            sym_e_contribution.append(subnet_out)
 
-            e_curr += self._output(x).output
+        # turns the list into another tensor
+        sym_preproc = tf.stack(sym_e_contribution, axis = 1)
 
-            # sum of all the atomic energy contributions
-            # non trainable (just a linear sum)
-            e_total = Dense(1, trainable = False)(e_curr)
-        return e_total
+        # add all the values in the tensor
+        return tf.reduce_sum(sym_preproc)
